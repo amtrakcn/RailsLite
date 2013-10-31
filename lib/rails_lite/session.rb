2eaf0fe1d2bddde95fcf807844eced40f7c46a4b
Charles Shen <scmfromnecf@gmail.com>
2013-10-31 11:08:30 -0700

    Update session.rb

--- lib/rails_lite/session.rb
+++ lib/rails_lite/session.rb
@@ -3,27 +3,22 @@ require 'webrick'
 
 class Session
   def initialize(req)
-  	@req = req
-  	@current_cookies = req.cookies
-  	@current_cookies.each do |cookie|
-  		@session = JSON.parse(cookie.value) if cookie.name == "_rails_lite_app"
-  	end
-  	@session ||= {}
-  	puts "session is #{@session}"
+    cookie = req.cookies.find { |request_cookie| request_cookie.name == "_rails_lite_app" }
+    @data = cookie.nil? ? {} : JSON.parse(cookie.value)
   end
 
   def [](key)
-  	puts "count is #{@session[key]}"
-  	@session[key]
+    @data[key]
   end
 
   def []=(key, val)
-  	@session[key] = val
+    @data[key] = val
   end
 
   def store_session(res)
-  	new_cookie = WEBrick::Cookie.new("_rails_lite_app", @session.to_json)
-  	puts "new cookie is #{new_cookie}"
-  	res.cookies << new_cookie
+    res.cookies << WEBrick::Cookie.new(
+      "_rails_lite_app",
+      @data.to_json
+    )
   end
 end
