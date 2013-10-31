a597013221698ecc794ec559a1be3beafb80cca3
Charles Shen <scmfromnecf@gmail.com>
2013-10-31 11:05:10 -0700

    updated to latest code

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -3,26 +3,51 @@ require 'uri'
 class Params
   def initialize(req, route_params)
     @params = {}
-    req.query_string
-
+    
+    @params.merge!(route_params)
+    if req.body
+      @params.merge!(parse_www_encoded_form(req.body))
+    end
+    if req.query_string
+      @params.merge!(parse_www_encoded_form(req.query_string))
+    end
   end
 
   def [](key)
+    @params[key]
   end
 
   def to_s
+    @params.to_json.to_s
   end
 
-  #private
-  def self.parse_www_encoded_form(www_encoded_form)
-    url_arr = URI::decode_www_form(www_encoded_form)
-    return nil if url_arr.nil?
-    url_arr.each do |param_pair|
-      @params[param_pair[0]] = param_pair[1]
+  private
+    params = {}
+
+    key_arr = URI.decode_www_form(www_encoded_form)
+    key_arr.each do |full_key, value|
+      scope = params
+
+      key_seq = parse_key(full_key)
+      key_seq.each_with_index do |key, index|
+        if (index + 1) == key_seq.count
+          scope[key] = value
+        else
+          scope[key] ||= {}
+          scope = scope[key]
+        end
+      end
     end
-    @params
+
+    params
   end
 
   def parse_key(key)
+    match_data = /(?<head>.*)\[(?<rest>.*)\]/.match(key)
+    if match_data
+      parse_key(match_data["rest"]).unshift(match_data["head"])
+    else
+      [key]
+    end
   end
 end
